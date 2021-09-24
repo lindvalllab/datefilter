5df50c462ac5fb8675b215b585b1a08c355b3083
Ben FL <benjamin_fayyazuddinljungberg@dfci.harvard.edu>
2021-09-24 11:32:55 -0400

    start functionality to filter csvs

---
+++ filter_data.py
@@ -0,0 +1,37 @@
+import csv
+import datetime
+from typing import Dict, Protocol, TextIO
+
+
+class DateInfo(Protocol):
+    anchor_date: datetime.date
+    days_before: datetime.timedelta
+    days_after: datetime.timedelta
+
+
+def include_row(row: Dict[str, str], date_info: Dict[str, DateInfo]) -> bool:
+    if row['EMPI'] not in date_info:
+        return True
+    patient_date_info = date_info[row['EMPI']]
+    first_date = patient_date_info.anchor_date - patient_date_info.days_before
+    last_date = patient_date_info.anchor_date + patient_date_info.days_after
+    stripped_date = row['Report_Date_Time'].split()[0]
+    row_date = datetime.datetime.strptime(stripped_date, '%m/%d/%Y').date()
+    return first_date <= row_date <= last_date
+
+
+def filter_data(input_file: TextIO, output_file: TextIO, date_info: Dict[str, DateInfo]) -> None:
+    reader = csv.DictReader(input_file)
+
+    # For the type-checker. The field names shouldn't be None.
+    field_names = reader.fieldnames if reader.fieldnames is not None else []
+    writer = csv.DictWriter(output_file, field_names)
+    writer.writeheader()
+    for row in reader:
+        if include_row(row, date_info):
+            writer.writerow(row)
+
+
+with open('../rpdr/out.csv') as file:
+    with open('new.csv', 'w') as file2:
+        filter_data(file, file2, {})
