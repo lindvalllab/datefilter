b3c612fcaa84417b6b0750bce7b68321843a2afe
Ben FL <benjamin_fayyazuddinljungberg@dfci.harvard.edu>
2021-10-04 14:48:16 -0400

    filter data: handle errors

--- filter_data.py
+++ filter_data.py
@@ -15,13 +15,30 @@ def include_row(row: Dict[str, str], date_info: Dict[str, DateInfo]) -> bool:
     return first_date <= row_date <= last_date
 
 
-def filter_data(input_file: TextIO, output_file: TextIO, date_info: Dict[str, DateInfo]) -> None:
+def filter_data(input_file: TextIO,
+                output_file: TextIO,
+                date_info: Dict[str, DateInfo],
+                append_error: Callable[[str], None]) -> None:
     reader = csv.DictReader(input_file)
 
     # For the type-checker. The field names shouldn't be None.
     field_names = reader.fieldnames if reader.fieldnames is not None else []
+    if 'EMPI' not in field_names:
+        append_error('The data file is missing an EMPI column.')
+        return
+    if 'Report_Date_Time' not in field names:
+        append_error('The data file is missing a Report_Date_Time column.')
+        return
+
     writer = csv.DictWriter(output_file, field_names)
     writer.writeheader()
     for row in reader:
-        if include_row(row, date_info):
-            writer.writerow(row)
+        if row['EMPI'] is None or row['Report_Date_Time'] is None:
+            append_error('Some rows are incomplete.')
+            return
+        try:
+            if include_row(row, date_info):
+                writer.writerow(row)
+        except ValueError:
+            append_error('The date ' + row['Report_Date_Time'] + ' is not of the '
+                         'form month/day/year.')
