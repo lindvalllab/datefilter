cf50c49fb680fa6f7aec9830161fbc92ff799902
Ben FL <benjamin_fayyazuddinljungberg@dfci.harvard.edu>
2021-11-10 16:32:28 -0500

    test include/exclude functionality

--- datefilter/test/test_datefilter.py
+++ datefilter/test/test_datefilter.py
@@ -28,3 +28,32 @@ def test_process(sample_name: str) -> None:
                        prefix + '_expected_output.csv')
 
     assert len(errors) == 0
+
+
+def test_process_include_exclude_missing() -> None:
+    errors = []
+
+    def append_error(error: str) -> None:
+        errors.append(error)
+
+    prefix = os.path.join(os.path.dirname(__file__), 'files/missing_patient')
+
+    process(prefix + '_data.csv',
+            prefix + '_filter.csv',
+            prefix + '_include_test_output.csv',
+            True,
+            append_error)
+
+    assert len(errors) == 0
+    assert filecmp.cmp(prefix + '_include_test_output.csv',
+                       prefix + '_include_expected_output.csv')
+
+    process(prefix + '_data.csv',
+            prefix + '_filter.csv',
+            prefix + '_exclude_test_output.csv',
+            False,
+            append_error)
+
+    assert len(errors) == 0
+    assert filecmp.cmp(prefix + '_exclude_test_output.csv',
+                       prefix + '_exclude_expected_output.csv')
