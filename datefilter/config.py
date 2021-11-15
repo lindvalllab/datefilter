e6513028e059ce6842ade347590e3dabf9198d89
Ben FL <benjamin_fayyazuddinljungberg@dfci.harvard.edu>
2021-11-15 16:03:40 -0500

    create functions to organize user config

--- datefilter/config.py
+++ datefilter/config.py
@@ -1,13 +1,37 @@
-from dataclasses import dataclass
+import json
+import dataclasses
+import os
 
 import appdirs
 
 
-@dataclass
+@dataclasses.dataclass
 class DatefilterConfig:
-    date_format: str
+    date_format: str = '%m/%d/%Y'
+    include_missing: bool = False
 
 
 def parse_config() -> DatefilterConfig:
-    appdirs.user_config_dir('datefilter')
-    return DatefilterConfig('%m/%d/%Y')
+    config_dir = appdirs.user_config_dir('datefilter')
+    config_path = os.path.join(config_dir, 'config.json')
+    try:
+        with open(config_path) as config_file:
+            config_dict = json.load(config_file)
+            return DatefilterConfig(**config_dict)
+    except Exception:
+        # Write a default config file.
+        write_config(DatefilterConfig())
+        return DatefilterConfig()
+
+
+def write_config(config: DatefilterConfig) -> None:
+    config_dir = appdirs.user_config_dir('datefilter')
+    config_path = os.path.join(config_dir, 'config.json')
+    os.makedirs(config_dir, exist_ok=True)
+    config_dict = dataclasses.asdict(config)
+    with open(config_path, 'w') as config_file:
+        json.dump(config_dict, config_file, indent=1)
+
+
+if __name__ == '__main__':
+    print(parse_config())
